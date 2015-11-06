c58303204251c831862eabed2c94d11edc399e0a
Himanshu Sharma <himanshu.sharma@shephertz.co.in>
2015-11-06 18:38:57 +0530

    initial commit
    Signed-off-by: Himanshu Sharma <himanshu.sharma@shephertz.co.in>

---
+++ Sample/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using com.shephertz.app42.gaming.multiplayer.client;
+
+namespace AppWarp_WP7_TestSDK
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            System.Diagnostics.Debug.WriteLine("Initializing Connection ###########");
+            
+            // Please update with the Api and Secret Key pair received when signing up.
+            WarpClient.initialize("9a7aa0b7b252e248d960bcf15da962f69293be42d85ff92cd27032b2373a4185", "450b40f7065716ac8cbe000f51dd2532a5938e6039518555a943f664be96d0ac");
+            //
+            
+            WarpClient game = WarpClient.GetInstance();
+            game.AddConnectionRequestListener(new ConListen(this));
+            game.AddZoneRequestListener(new ZoneReqListener(this));
+            game.AddRoomRequestListener(new RoomReqListener(this));
+            game.AddNotificationListener(new NotificationListener(this));
+            game.AddLobbyRequestListener(new LobbyReqListen(this));
+            //game.Connect();
+        }
+
+        public void showResult(String result)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                resutlBlock.Text = result;
+            });
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            WarpClient.GetInstance().Connect(inputBox.Text);
+        }
+
+        private void joinButton_Click(object sender, RoutedEventArgs e)
+        {
+            WarpClient.GetInstance().JoinRoom(this.inputJoinRoomIdBox.Text);
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {            
+            Dictionary<string,object> tabelProperties = new Dictionary<string,object>();
+            WarpClient.GetInstance().CreateRoom(inputCreateRoomBox.Text, inputBox.Text, 5, tabelProperties);
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Sample/NotificationListener.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using com.shephertz.app42.gaming.multiplayer.client.events;
+using com.shephertz.app42.gaming.multiplayer.client.command;
+using com.shephertz.app42.gaming.multiplayer.client;
+using System.Collections.Generic;
+
+namespace AppWarp_WP7_TestSDK
+{
+    public class NotificationListener : com.shephertz.app42.gaming.multiplayer.client.listener.NotifyListener
+    {
+        private MainPage _page;
+
+        public NotificationListener(MainPage page)
+        {
+            _page = page;
+        }
+
+        public void onNextTurnRequest(string lastTurn) { 
+        }
+
+        public void onPrivateUpdateReceived(string sender, byte[] update, bool fromUdp) { 
+        }
+        public void onGameStopped(string sender, string roomId)
+        { 
+        }
+        public void onGameStarted(string sender, string roomId, string nextTurn)
+        { 
+        }
+        public void onUserResumed(string locid, bool isLobby, string username)
+        { 
+        }
+        public void onUserPaused(string locid, bool isLobby, string username)
+        { 
+        }
+        public void onMoveCompleted(MoveEvent moveEvent)
+        { 
+        }
+        public void onPrivateChatReceived(string sender, string message)
+        { 
+        }
+        public void onUserChangeRoomProperty(RoomData roomData, string sender, Dictionary<string, object> properties, Dictionary<string, string> lockedPropertiesTable)
+        { 
+        }
+
+        public void onRoomCreated(RoomData eventObj)
+        {
+        }
+        public void onRoomDestroyed(RoomData eventObj)
+        {
+        }
+        public void onUserLeftRoom(RoomData eventObj, String username)
+        {
+        }
+        public void onUserJoinedRoom(RoomData eventObj, String username)
+        {
+            _page.showResult(username + " joined " + eventObj.getId());
+        }
+
+        public void onUserLeftLobby(LobbyData eventObj, String username)
+        {
+        }
+        public void onUserJoinedLobby(LobbyData eventObj, String username)
+        {
+        }
+
+        public void onChatReceived(ChatEvent eventObj)
+        {
+            _page.showResult("chat from " + eventObj.getSender() + " msg " + eventObj.getMessage() + " id "+eventObj.getLocationId() + eventObj.isLocationLobby());
+            WarpClient.GetInstance().GetLiveLobbyInfo();
+        }
+        public void onUpdatePeersReceived(UpdateEvent eventObj)
+        {
+            string j = System.Text.UTF8Encoding.UTF8.GetString(eventObj.getUpdate(), 0, eventObj.getUpdate().Length);
+            _page.showResult("update recvd " + j );
+        }
+        public void onUserChangeRoomProperty(RoomData roomData, string sender, Dictionary<String, Object> properties)
+        {
+            _page.showResult("Notification for User Changed Room Propert received");
+            _page.showResult(roomData.getId());
+            _page.showResult(sender);
+            foreach (KeyValuePair<string, object> entry in properties)
+            {
+                _page.showResult("KEY:" + entry.Key);
+                _page.showResult("VALUE:" + entry.Value.ToString());
+            }
+        }
+    }
+}
